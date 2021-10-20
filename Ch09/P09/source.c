caa83267cc5f4ceabdc8a516dfa442d9a98a6304
SeungJu <realseungju@naver.com>
2021-10-20 22:13:37 +0900

    Practice

---
+++ Ch09/P09/source.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int add_digit(int x);
+
+int main()
+{
+  int x;
+  int result = 0;
+  printf("정수를 입력하시오: ");
+  scanf("%d", &x);
+  printf("자리수의 합 : %d", add_digit(x));
+
+  return 0;
+}
+
+int add_digit(int x)
+{
+
+  if (x/10 > 0)
+  {
+    x = x%10 + add_digit(x/10);
+  }
+
+  return x;
+
+}
