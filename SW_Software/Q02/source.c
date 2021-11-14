a4b56494387c4dbada7b73cfdda5b504b142bff2
realseungju <92769406+realseungju@users.noreply.github.com>
2021-11-15 08:19:52 +0900

    Practice
    Microwave question.
    Display minimum button press value.
    5 min, 1 min, 10 sec.

---
+++ SW_Software/Q02/source.c
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    int time;
+    int sec_count;
+    int min_count;
+    int five_min_count;
+
+    while(1)
+    {
+        sec_count = 0;
+        min_count = 0;
+        five_min_count = 0;
+
+        scanf("%d",&time);
+
+        if(time>=300)
+        {
+            five_min_count = time/300; // 몫은 카운트
+            time = time%300; // 나머지를 time에 저장
+        }
+
+        if(time>=60)
+        {
+            min_count = time/60;
+            time = time%60;
+        }
+
+        if(time>=10)
+        {
+            sec_count = time/10;
+            time = time%10;
+        }
+
+        if(time>0) // 10으로 나눈 나머지가 남아있으면
+        {
+            printf("%d\n",-1); // -1 출력
+        }
+        else
+        {
+            printf("%3d %3d %3d\n",five_min_count,min_count,sec_count); // 5분 1분 10초
+        }
+    }
+
+    return 0;
+}
